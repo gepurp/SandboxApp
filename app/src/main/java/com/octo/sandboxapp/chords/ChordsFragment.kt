da044507c521c3ca3f289590a1034f09b7468f02
pkonstantinov <pkonstantinov@tradingview.com>
2021-03-10 09:41:16 -0800

    Add navigation and inflating fragments with binding

--- app/src/main/java/com/octo/sandboxapp/chords/ChordsFragment.kt
+++ app/src/main/java/com/octo/sandboxapp/chords/ChordsFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.octo.sandboxapp.R
+import com.octo.sandboxapp.databinding.FragmentChordsBinding
 
 class ChordsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chords, container, false)
+    ): View {
+
+        val binding = DataBindingUtil.inflate<FragmentChordsBinding>(
+            inflater,
+            R.layout.fragment_chords,
+            container,
+            false
+        )
+
+        // Test fragment transition
+        binding.tvTestChordsText.setOnClickListener {
+            it.findNavController().navigate(R.id.action_chordsFragment_to_menuFragment)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/octo/sandboxapp/menu/MenuFragment.kt
+++ app/src/main/java/com/octo/sandboxapp/menu/MenuFragment.kt
@@ -1,19 +1,34 @@
 package com.octo.sandboxapp.menu
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.octo.sandboxapp.R
+import com.octo.sandboxapp.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+    ): View {
+
+        val binding = DataBindingUtil.inflate<FragmentMenuBinding>(
+            inflater,
+            R.layout.fragment_menu,
+            container,
+            false
+        )
+
+        // Test fragment transition
+        binding.tvTestMenuText.setOnClickListener {
+            it.findNavController().navigate(R.id.action_menuFragment_to_chordsFragment)
+        }
+
+        return binding.root
     }
 }
