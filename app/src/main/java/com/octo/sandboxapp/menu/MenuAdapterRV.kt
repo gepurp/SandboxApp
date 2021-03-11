c23f5369334c1cbd32cc3a9e034dfd0d1a87a9d9
pkonstantinov <pkonstantinov@tradingview.com>
2021-03-10 22:21:58 -0800

    Add empty RV and adapter

---
+++ app/src/main/java/com/octo/sandboxapp/menu/MenuAdapterRV.kt
@@ -0,0 +1,23 @@
+package com.octo.sandboxapp.menu
+
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MenuAdapterRV : RecyclerView.Adapter<MenuItemViewHolder>() {
+
+    // TODO: Add list of menu items
+    var itemsList = listOf<TextView>()
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MenuItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/octo/sandboxapp/menu/MenuItemViewHolder.kt
@@ -0,0 +1,6 @@
+package com.octo.sandboxapp.menu
+
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MenuItemViewHolder(private val textView: TextView) : RecyclerView.ViewHolder(textView)
