bfaf75a275c5a6122a81adaac42e3bb9fbc6f38c
pkonstantinov <pkonstantinov@tradingview.com>
2021-03-10 22:06:13 -0800

    Add some test data and some questions

--- app/src/main/java/com/octo/sandboxapp/menu/MenuFragment.kt
+++ app/src/main/java/com/octo/sandboxapp/menu/MenuFragment.kt
@@ -1,11 +1,15 @@
 package com.octo.sandboxapp.menu
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.transition.TransitionInflater
 import com.octo.sandboxapp.R
@@ -13,12 +17,16 @@ import com.octo.sandboxapp.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
 
+    private lateinit var menuFragmentBinding: FragmentMenuBinding
+
+    private lateinit var menuViewModel: MenuViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        val binding = DataBindingUtil.inflate<FragmentMenuBinding>(
+        val menuFragmentBinding = DataBindingUtil.inflate<FragmentMenuBinding>(
             inflater,
             R.layout.fragment_menu,
             container,
@@ -26,11 +34,29 @@ class MenuFragment : Fragment() {
         )
 
         // Test fragment transition
-        binding.tvTestMenuText.setOnClickListener {
+        menuFragmentBinding.tvTestMenuText.setOnClickListener {
             it.findNavController().navigate(R.id.action_menuFragment_to_chordsFragment)
         }
 
-        return binding.root
+        Log.i("MenuFragment", "Called ViewModelProvider.get")
+        menuViewModel = ViewModelProvider(this).get(MenuViewModel::class.java)
+
+        // Test observer
+        menuViewModel.testClickCount.observe(viewLifecycleOwner, Observer { clickCount ->
+            if (clickCount % 10 == 0) {
+                showToastMessage("You clicked the item ten times")
+            }
+        })
+
+        return menuFragmentBinding.root
+    }
+
+    private fun showToastMessage(message: String) {
+        Toast.makeText(
+            context,
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/octo/sandboxapp/menu/MenuViewModel.kt
+++ app/src/main/java/com/octo/sandboxapp/menu/MenuViewModel.kt
@@ -1,12 +1,25 @@
 package com.octo.sandboxapp.menu
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MenuViewModel : ViewModel() {
 
+    // TODO: How to store liveData value when switch the fragment?
+    private val _testClickCount = MutableLiveData<Int>()
+    val testClickCount: LiveData<Int>
+        get() = _testClickCount
+
     init {
+        _testClickCount.value = 10
         Log.i("MenuViewModel", "MenuViewModel created...")
+    }
+
+    // Call when recycler item will be clicked
+    fun onRecyclerItemClick() {
+        _testClickCount.value?.plus(1)
     }
 
     override fun onCleared() {
