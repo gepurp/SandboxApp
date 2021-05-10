dae0c0b66dab3d13168fc54b56cf935d3de33fbd
pkonstantinov <pkonstantinov@tradingview.com>
2021-05-10 09:57:29 -0700

    Add nav drawer to main screen

--- app/src/main/java/com/octo/sandboxapp/activity/MainActivity.kt
+++ app/src/main/java/com/octo/sandboxapp/activity/MainActivity.kt
@@ -2,11 +2,37 @@ package com.octo.sandboxapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.octo.sandboxapp.R
+import com.octo.sandboxapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding =
+            DataBindingUtil
+                .setContentView<ActivityMainBinding>(
+                    this,
+                    R.layout.activity_main
+                )
+
+        drawerLayout = binding.drawerLayout
+        navController = this.findNavController(R.id.mainNavHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
